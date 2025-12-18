ad664b24b649c625de712fa16e127e8ab66f65bc
arbitrarycodeexecution <570349@escg.ac.uk>
2025-12-18 13:55:01 +0000

    DeathScreen
    Death screen

---
+++ Assets/Scripts/ManageScenes.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ManageScenes : MonoBehaviour
+{
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public static void LoadGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    public static void LoadMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
