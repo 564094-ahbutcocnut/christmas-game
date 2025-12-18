6fa61b144ebd3360544a66a557f1c3713a0cba53
Qwerty121240k <534402@chrome.escg.ac.uk>
2025-12-18 10:13:00 +0000

    menu

---
+++ Assets/-project/scripts/menuManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menuManager : MonoBehaviour
+{
+    public string scenename;
+    public void sceneswap()
+    {
+        SceneManager.LoadScene(scenename);
+
+    }
+
+    public void Quit()
+    {
+        
+    }
+
+}
